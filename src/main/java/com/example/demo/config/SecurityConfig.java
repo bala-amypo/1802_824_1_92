c6fd507f90aa480f01ab0ba7d3aa7046e8a2b970
bala-amypo <bala@amypo.in>
2025-12-17 06:29:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class Securityconfig {
+
+    @Bean
+    public class
+}
