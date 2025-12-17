ff7df1aa8d9c7ec665b2c7bea18bf925f9160f6d
bala-amypo <bala@amypo.in>
2025-12-17 04:59:51 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtFilter.java
+++ src/main/java/com/example/demo/config/JwtFilter.java
@@ -1,1 +1,16 @@
-package
+package com.example.demo.config;
+
+import io.jsonwebtoken.*;
+import java.util.Date;
+
+public class JwtUtil {
+
+    private static final String SECRET_KEY =  "mySecretKey";
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hour;
+
+    // Generate JWT
+    public static String generateToken(String username){
+        return Jwts.builder()
+        .setSubject(username)
+    }
+}
