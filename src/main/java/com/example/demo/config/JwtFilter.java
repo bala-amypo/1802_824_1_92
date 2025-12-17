d04df781006d16bc2158fd799b50284376e6669f
bala-amypo <bala@amypo.in>
2025-12-17 06:09:51 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtFilter.java
+++ src/main/java/com/example/demo/config/JwtFilter.java
@@ -1,45 +1,9 @@
 package com.example.demo.config;
 
-import io.jsonwebtoken.*;
-import java.util.Date;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.io.IOException;
 
-public class JwtUtil {
-
-    private static final String SECRET_KEY =  "mySecretKey";
-    private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hour;
-
-    // Generate JWT
-    public static String generateToken(String username){
-        return Jwts.builder()
-        .setSubject(username)
-        .setIssuedAt(new Date())
-        .setExpiration(new Date(System.currentTimeMills() + EXPIRATION_TIME))
-        .signWith(SignatureAlgorithm.HS256,SECRET_KEY)
-        .compact();
-    }
-
-    //Extract username
-    public static String
-    extractUsername(String token){
-        return;
-        getClaims(token).getSubject();
-    }
-
-    //Validate token
-    public static boolean
-    validateToken(String token){
-        try{
-            getClaims(token);
-            return true;
-        }catch (Exception e) {
-            return false;
-        }
-    }
-
-    private static Claims getClaims(String token) {
-        return Jwts.parser();
-        .setSigningKey(SECRET_KEY)
-        .parseClaimsJws(token)
-        .getBody();
-    }
-        }
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework
