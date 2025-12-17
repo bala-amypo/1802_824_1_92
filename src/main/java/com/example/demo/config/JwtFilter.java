9ef8757f5f4a4cda58ec55112f286dba5df896cb
bala-amypo <bala@amypo.in>
2025-12-17 06:24:52 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtFilter.java
+++ src/main/java/com/example/demo/config/JwtFilter.java
@@ -24,8 +24,13 @@ public class JwtFilter extends OncePerRequestFilter {
                 String token = header.substring(7);
 
                 if (JwtUtil.validateToken(token)) {
-                    String username = JwtUtil.extractUsername
+                    String username = JwtUtil.extractUsername(token);
+
+                    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, null,null);
+
+                    SecurityContextHolder.getContext().setAuthentication(auth);
                 }
             }
+            filterChain.doFilter(request,response);
         }
 }
