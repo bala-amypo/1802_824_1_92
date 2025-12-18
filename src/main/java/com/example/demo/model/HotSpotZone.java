41a4f42e00281e4bd41dd3e45b0f0666a6ce23bb
bala-amypo <bala@amypo.in>
2025-12-18 10:29:50 +0000

    User commit

--- src/main/java/com/example/demo/model/HotSpotZone.java
+++ src/main/java/com/example/demo/model/HotSpotZone.java
@@ -1,12 +1,11 @@
 package com.example.demo.example;
 
-import java.time.LocalDateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
-public class HotSpotzone {
+public class HotSpotZone {
 
     @Id
     private Long Id;
@@ -16,5 +15,63 @@ public class HotSpotzone {
     private String zoneName;
     private Double centerLat;
     private Double centerlog;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSeverityLevel() {
+        return severityLevel;
+    }
+
+    public void setSeverityLevel(String severityLevel) {
+        this.severityLevel = severityLevel;
+    }
+
+    public String getZoneName() {
+        return zoneName;
+    }
+
+    public void setZoneName(String zoneName) {
+        this.zoneName = zoneName;
+    }
+
+    public Double getCenterLat() {
+        return centerLat;
+    }
+
+    public void setCenterLat(Double centerLat) {
+        this.centerLat = centerLat;
+    }
+
+    public Double getCenterlog() {
+        return centerlog;
+    }
+
+    public void setCenterlog(Double centerlog) {
+        this.centerlog = centerlog;
+    }
+
+    public HotSpotzone(Long id, String name, String severityLevel, String zoneName, Double centerLat,
+            Double centerlog) {
+        Id = id;
+        this.name = name;
+        this.severityLevel = severityLevel;
+        this.zoneName = zoneName;
+        this.centerLat = centerLat;
+        this.centerlog = centerlog;
+    }
     
 }
