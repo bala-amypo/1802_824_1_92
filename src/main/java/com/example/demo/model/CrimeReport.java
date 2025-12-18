a6946f2701299717c675456897f133233182cfb7
bala-amypo <bala@amypo.in>
2025-12-18 09:14:51 +0000

    User commit

--- src/main/java/com/example/demo/model/CrimeReport.java
+++ src/main/java/com/example/demo/model/CrimeReport.java
@@ -1,4 +1,4 @@
-package.com.example.demo.model;
+package com.example.demo.model;
 
 import java.time.LocalDateTime;
 import jakarta.persistence.Column;
@@ -17,6 +17,72 @@ public class CrimeReport {
     private Double latitude;
     private Double longitude;
     private LocalDateTime createdAt;
+
+    public CrimeReport(Long Id, LocalDateTime createdAt, String crimeType, String description, Double latitude, Double longitude, String name) {
+        this.Id = Id;
+        this.createdAt = createdAt;
+        this.crimeType = crimeType;
+        this.description = description;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCrimeType() {
+        return crimeType;
+    }
+
+    public void setCrimeType(String crimeType) {
+        this.crimeType = crimeType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 
     
 }
