408764d65ab2069db43a9df62445416973b55b25
bala-amypo <bala@amypo.in>
2025-12-18 10:14:50 +0000

    User commit

--- src/main/java/com/example/demo/model/PatternDetectionResult.java
+++ src/main/java/com/example/demo/model/PatternDetectionResult.java
@@ -1,4 +1,19 @@
 package com.example.demo.model;
 
 import java.time.LocalDateTime;
-import jakarta.persistence.
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class PatternDetectionResult {
+
+    @Id
+    private Long Id;
+    private String name;
+    @Column(unique=true)
+    private String detectedPattern;
+    private String zone;
+    private int crimeCount;
+    private LocalDateTime createdAt;
+}
