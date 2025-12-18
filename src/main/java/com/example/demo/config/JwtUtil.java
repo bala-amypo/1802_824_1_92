535e532f4c19fca2be4d3953058e59718242afed
bala-amypo <bala@amypo.in>
2025-12-18 04:27:15 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtUtil.java
+++ src/main/java/com/example/demo/config/JwtUtil.java
@@ -1,33 +1,83 @@
 package com.example.demo.config;
 
-private static final String SECRET_KEY = "mySecretKey";
-private stati final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hour
 
-// Generate JWT
-public static String generateToken(String username) {
-    return Jwts.builder()
-    .setSubject(username)
-    .setIssuedAt(new Data())
-    .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-    .signWith(SignatureAlgorithm.HS256,SECRET_KEY)
-    .compact();
-}
 
-// Validate token
-public static boolean 
-ValidateToken(String token){
-    try{
-        getClaims(token);
-        return true;
-    } catch (Exception e) {
-        return false;
-    }
-}
+import io.jsonwebtoken.*;
 
-private static Claims getClaims(String token) {
-    return Jwts.parser()
-    .setSigningKey(SECRET_KEY)
-    .parseClaimsJws(token)
-    .getbody();
-}
-}
+import org.springframework.stereotype.Component;
+
+
+
+import java.util.Date;
+
+
+
+@Component
+
+public class JwtUtil {
+
+
+
+    private final String SECRET_KEY = "secretkey123";
+
+    private final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hour
+
+
+
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+
+        .setSubject(username)
+
+        .setIssuedAt(new Date())
+
+        .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+
+        .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+
+        .compact();
+
+        }
+
+
+
+        public String extractUsername(String token) {
+
+            return getClaims(token).getSubject();
+
+            }
+
+
+
+            public boolean isTokenValid(String token) {
+
+                try {
+
+                getClaims(token);
+
+                return true;
+
+                } catch (Exception e) {
+
+                    return false;
+
+                    }
+
+                    }
+
+
+
+                    private Claims getClaims(String token) {
+
+                    return Jwts.parser()
+
+                    .setSigningKey(SECRET_KEY)
+
+                    .parseClaimsJws(token)
+
+                    .getBody();
+
+                    }
+
+                    }
