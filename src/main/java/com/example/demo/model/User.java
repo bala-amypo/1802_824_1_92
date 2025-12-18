2cf47d927d002352205fe895349d106ce14c9520
bala-amypo <bala@amypo.in>
2025-12-18 08:48:11 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,9 +1,9 @@
 package com.example.demo.model;
 
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Column;
-import java.time.LocalDateTime;
 
 @Entity
 public class User {
@@ -17,7 +17,65 @@ public class User {
     private String role;
     private LocalDateTime createdAt;
 
+    public User(LocalDateTime createdAt, String email, Long id, String name, String password, String role) {
+        this.createdAt = createdAt;
+        this.email = email;
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.role = role;
+    }
+
     public void ValidateAndInitializer() {
         this.createdAt = LocalDateTime.now(); 
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    
 }
