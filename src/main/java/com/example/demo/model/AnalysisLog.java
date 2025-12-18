acb317031662fecd398dc07f274e079dd01f0930
bala-amypo <bala@amypo.in>
2025-12-18 09:29:50 +0000

    User commit

--- src/main/java/com/example/demo/model/AnalysisLog.java
+++ src/main/java/com/example/demo/model/AnalysisLog.java
@@ -5,3 +5,15 @@ import jakarta.persistence.column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+@Entity
+public class AnalysisLog {
+
+    @Id
+    private Long Id;
+    private String name;
+    @Column(unique=true)
+    private String message;
+    private String zone;
+    private LocalDateTime createdAt;
+
+}
