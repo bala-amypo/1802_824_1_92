38e8cbcbf16270eca607657ac53a83acb0b41be8
bala-amypo <bala@amypo.in>
2025-12-18 09:04:51 +0000

    User commit

--- src/main/java/com/example/demo/model/CrimeReport.java
+++ src/main/java/com/example/demo/model/CrimeReport.java
@@ -6,3 +6,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
+public class CrimeReport {
+
+    @Id
+    private Long Id;
+    private String name;
+    @Column(unique=true)
+    private String crimeType;
+    private String description;
+    private Double latitude;
+    private Double longitude;
+    private LocalDateTime createdAt
+}
