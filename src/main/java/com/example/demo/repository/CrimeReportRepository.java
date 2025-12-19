f203218790fb99bc414e7a6dfe228dbd28b71d3f
bala-amypo <bala@amypo.in>
2025-12-19 04:04:24 +0000

    User commit

--- src/main/java/com/example/demo/repository/CrimeReportRepository.java
+++ src/main/java/com/example/demo/repository/CrimeReportRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.CrimeReport;
+import com.example.demo.model.CrimeReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CrimeReportRepository extends JpaRepository<CrimeReport, Long>{
