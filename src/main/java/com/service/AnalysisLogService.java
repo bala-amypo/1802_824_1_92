cc5faf54a198f7ba4b1f6afd2b7b71cfac2e3f19
bala-amypo <bala@amypo.in>
2025-12-19 05:25:54 +0000

    User commit

--- src/main/java/com/service/AnalysisLogService.java
+++ src/main/java/com/service/AnalysisLogService.java
@@ -3,8 +3,8 @@ import jakarta.persistence.service;
 import com.example.demo.model.AnalysisLog;
 @Service
 public interface AnalysisLogService{
-    public AnalysisLog createAnalysisLog(AnalysisLog AnalyisLog);
+    public AnalysisLog createAnalysisLog(Analy
 
-    public AnalysisLog addLog(Long zoneId, String message)
-    public AnalysisLog 
+    public AnalysisLog addLog(Long zoneId, String message);
+    public List<Analysis
 }
