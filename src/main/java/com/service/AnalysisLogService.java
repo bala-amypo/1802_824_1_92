5c554d1119291b2719d4c4b0b70ddbdf680bbda0
bala-amypo <bala@amypo.in>
2025-12-19 05:19:54 +0000

    User commit

--- src/main/java/com/service/AnalysisLogService.java
+++ src/main/java/com/service/AnalysisLogService.java
@@ -1,4 +1,10 @@
 package com.example.demo.service;
 import jakarta.persistence.service;
-import com.example.demo.model.visitor;
+import com.example.demo.model.AnalysisLog;
 @Service
+public interface AnalysisLogService{
+    public AnalysisLog createAnalysisLog(AnalysisLog AnalyisLog);
+
+    public AnalysisLog getAnalysisLog(Long id);
+    public AnalysisLog getA
+}
