e713fa6218652132ff5acd5718ef95874d631483
bala-amypo <bala@amypo.in>
2025-12-19 08:37:54 +0000

    User commit

--- src/main/java/com/service/CrimeReportService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-import jakarta.persistence.service;
-import com.example.demo.model.CrimeReport;
-@Service
-public interface CrimeReportService{
-
-    public CrimeReport addReport(CrimeReport report);
-    public CrimeReport List<CrimeReport> getAllReports();
-}
