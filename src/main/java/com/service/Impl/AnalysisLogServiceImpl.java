0923f896e52ae67bd9dfe90225776580bb7fa9e9
bala-amypo <bala@amypo.in>
2025-12-19 06:19:54 +0000

    User commit

--- src/main/java/com/service/Impl/AnalysisLogServiceImpl.java
+++ src/main/java/com/service/Impl/AnalysisLogServiceImpl.java
@@ -1,11 +1,12 @@
 
-
+@Service
 public class AnalysisLogServiceImpl implements AnalysisLogService{
 
-
+@Autowired
+AnalysisLogRepository repo;
 public AnalysisLog addLog(AnalysisLog logg){
 
-    
+return repo.save(logg);
 }
 
 
