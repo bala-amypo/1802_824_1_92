e9dec51876ccd93c57eb49c13250db7eaebbf0ec
bala-amypo <bala@amypo.in>
2025-12-19 06:31:54 +0000

    User commit

--- src/main/java/com/service/Impl/AnalysisLogServiceImpl.java
+++ src/main/java/com/service/Impl/AnalysisLogServiceImpl.java
@@ -2,6 +2,13 @@ package com.example.demo.service;
 
 import java.time.LocalDatetime;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.AnalysisLog;
+import com.example.demo.repository.AnalysisLogRepository;
+
 @Service
 public class AnalysisLogServiceImpl implements AnalysisLogService{
 
