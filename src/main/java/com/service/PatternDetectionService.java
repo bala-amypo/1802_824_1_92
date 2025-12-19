4d9760e957adaf95382adf877279154ae0a9207b
bala-amypo <bala@amypo.in>
2025-12-19 06:04:54 +0000

    User commit

--- src/main/java/com/service/PatternDetectionService.java
+++ src/main/java/com/service/PatternDetectionService.java
@@ -4,6 +4,6 @@ import com.example.demo.model.PatternDetection;
 @Service
 public interface PatternDetectionService{
 
-    public PatternDetection 
+    public PatternDetection PatternDetectionResult detectPattern
     public PatternDetection 
 }
