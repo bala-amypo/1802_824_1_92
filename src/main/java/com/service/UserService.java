dae9421bd22c6ce3f61107d1c8629f2203999183
bala-amypo <bala@amypo.in>
2025-12-19 06:10:54 +0000

    User commit

--- src/main/java/com/service/UserService.java
+++ src/main/java/com/service/UserService.java
@@ -1,9 +1,9 @@
 package com.example.demo.service;
 import jakarta.persistence.service;
-import com.example.demo.model.PatternDetection;
+import com.example.demo.model.User;
 @Service
-public interface PatternDetectionService{
+public interface UserService{
 
-    public 
-    public 
+    public UserService User register(User user); 
+    public UserService User findByEmail(String email);
     }
