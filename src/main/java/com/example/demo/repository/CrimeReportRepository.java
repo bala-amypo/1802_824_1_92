3af66d942bbd3d263327f9259f56e70c589fd42e
bala-amypo <bala@amypo.in>
2025-12-22 09:51:37 +0000

    User commit

--- src/main/java/com/example/demo/repository/CrimeReportRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.CrimeReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CrimeReportRepository extends JpaRepository<CrimeReport, Long> {
-    // JpaRepository provides save(), findAll(), findById(), delete(), etc.
-}
