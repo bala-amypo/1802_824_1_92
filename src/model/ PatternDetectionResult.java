2626b12da665a12bb7986dee518841fe1de3a875
bala-amypo <bala@amypo.in>
2025-12-23 04:27:36 +0000

    User commit

--- src/model/ PatternDetectionResult.java
+++ src/model/ PatternDetectionResult.java
@@ -8,30 +8,5 @@ public class PatternDetectionResult {
 
     @Id
     private Long id;
-    private String patternType;
-    private String description;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getPatternType() {
-        return patternType;
-    }
-
-    public void setPatternType(String patternType) {
-        this.patternType = patternType;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    private String result;
 }
