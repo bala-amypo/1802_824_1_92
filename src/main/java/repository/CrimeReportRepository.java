adb4e29ca8e752c34c2e80c88366c619ee96e3df
bala-amypo <bala@amypo.in>
2025-12-23 05:00:35 +0000

    User commit

--- src/main/java/repository/CrimeReportRepository.java
+++ src/main/java/repository/CrimeReportRepository.java
@@ -1,13 +1,3 @@
-// package com.example.demo.repository;
-
-// import com.example.demo.entity.CrimeReport;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-
-// @Repository
-// public interface CrimeReportRepository extends JpaRepository<CrimeReport, Long> {
-// }
-
 
 
 package com.example.demo.repository;
--- src/main/java/repository/PatternDetectionResultRepository.java
+++ src/main/java/repository/PatternDetectionResultRepository.java
@@ -1,16 +1,4 @@
-// package com.example.demo.repository;
-
-// import com.example.demo.entity.PatternDetectionResult;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-
-// @Repository
-// public interface PatternDetectionResultRepository
-//         extends JpaRepository<PatternDetectionResult, Long> {
-// }
-
-
-
+ package com.example.demo.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
