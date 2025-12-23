2bd87d5ad0ae980afc1001eeee37d446b40d10b9
bala-amypo <bala@amypo.in>
2025-12-23 04:54:35 +0000

    User commit

--- src/main/java/repository/CrimeReportRepository.java
+++ src/main/java/repository/CrimeReportRepository.java
@@ -1,9 +1,20 @@
+// package com.example.demo.repository;
+
+// import com.example.demo.entity.CrimeReport;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+
+// @Repository
+// public interface CrimeReportRepository extends JpaRepository<CrimeReport, Long> {
+// }
+
+
+
 package com.example.demo.repository;
 
-import com.example.demo.entity.CrimeReport;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import com.example.demo.entity.CrimeReport;
 
-@Repository
-public interface CrimeReportRepository extends JpaRepository<CrimeReport, Long> {
-}
+public interface CrimeReportRepository extends JpaRepository<CrimeReport, Long> {}
+
+
--- src/main/java/repository/HotspotZoneRepository.java
+++ src/main/java/repository/HotspotZoneRepository.java
@@ -1,7 +1,25 @@
+// package com.example.demo.repository;
+
+// import com.example.demo.entity.HotspotZone;
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// public interface HotspotZoneRepository extends JpaRepository<HotspotZone, Long> {
+// }
+
+
+
+UserRepository.java
 package com.example.demo.repository;
 
-import com.example.demo.entity.HotspotZone;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.User;
 
-public interface HotspotZoneRepository extends JpaRepository<HotspotZone, Long> {
-}
+public interface UserRepository extends JpaRepository<User, Long> {}
+
+HotspotZoneRepository.java
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.HotspotZone;
+
+public interface HotspotZoneRepository extends JpaRepository<HotspotZone, Long> {}
