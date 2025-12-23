fe0909afb4b88114503fbf08dbc7352b603ba7db
bala-amypo <bala@amypo.in>
2025-12-23 04:24:35 +0000

    User commit

--- src/model/ HotspotZone.java
+++ src/model/ HotspotZone.java
@@ -9,29 +9,4 @@ public class HotspotZone {
     @Id
     private Long id;
     private String zoneName;
-    private String riskLevel;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getZoneName() {
-        return zoneName;
-    }
-
-    public void setZoneName(String zoneName) {
-        this.zoneName = zoneName;
-    }
-
-    public String getRiskLevel() {
-        return riskLevel;
-    }
-
-    public void setRiskLevel(String riskLevel) {
-        this.riskLevel = riskLevel;
-    }
 }
