5bfdc41e8b15b956b15585d44ae027d0bb433d3f
bala-amypo <bala@amypo.in>
2025-12-26 15:16:58 +0000

    User commit

--- src/main/java/com/example/demo/service/CrimeReportService.java
+++ src/main/java/com/example/demo/service/CrimeReportService.java
@@ -1,25 +1,12 @@
 package com.example.demo.service;
 
 import com.example.demo.model.CrimeReport;
-import com.example.demo.repository.CrimeReportRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Service
-public class CrimeReportService {
+public interface CrimeReportService {
 
-    private final CrimeReportRepository repository;
+    CrimeReport createCrime(CrimeReport crimeReport);
 
-    public CrimeReportService(CrimeReportRepository repository) {
-        this.repository = repository;
-    }
-
-    public CrimeReport createCrime(CrimeReport crimeReport) {
-        return repository.save(crimeReport);
-    }
-
-    public Optional<CrimeReport> getCrimeById(Long id) {
-        return repository.findById(id);
-    }
+    Optional<CrimeReport> getById(Long id);
 }
