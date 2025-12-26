1c3942adf4e88a6518b0d770a4b45a654a4a22ad
bala-amypo <bala@amypo.in>
2025-12-26 17:24:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/HotspotZoneService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HotspotZoneService {
+
+    public String getHotspots() {
+        return "Hotspot zones data";
+    }
+}
