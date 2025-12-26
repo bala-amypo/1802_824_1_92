b61be330995df4efd9a38d3916ab4365aaba41b7
bala-amypo <bala@amypo.in>
2025-12-26 17:03:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/AnalysisLogService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import com.example.demo.model.AnalysisLog;
+import java.util.List;
+
+public interface AnalysisLogService {
+    AnalysisLog save(AnalysisLog log);
+    List<AnalysisLog> getAll();
+}
---
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository repo;
+
+    public UserServiceImpl(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return repo.save(user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return repo.findAll();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return repo.findById(id).orElse(null);
+    }
+}
