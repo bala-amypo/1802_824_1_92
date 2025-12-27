5489636506f63f476e26a51e55310f0e5b0b0d64
bala-amypo <bala@amypo.in>
2025-12-27 04:25:13 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,35 +1,24 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-
-@Entity
 public class User {
 
-    @Id
     private Long id;
-    private String username;
+    private String email;
     private String password;
-    private String role;
 
     public User() {}
 
-    public User(Long id, String username, String password, String role) {
-        this.id = id;
-        this.username = username;
+    public User(String email, String password) {
+        this.email = email;
         this.password = password;
-        this.role = role;
     }
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
-
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
 }
