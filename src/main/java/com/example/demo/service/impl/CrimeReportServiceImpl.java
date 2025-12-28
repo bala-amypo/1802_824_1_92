633b7d6ccf319493f7dfe905693bda0a51f4f959
bala-amypo <bala@amypo.in>
2025-12-28 16:56:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CrimeReportServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CrimeReportServiceImpl.java
@@ -1,11 +1,12 @@
 package com.example.demo.service.impl;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
 import com.example.demo.model.CrimeReport;
 import com.example.demo.repository.CrimeReportRepository;
 import com.example.demo.service.CrimeReportService;
-import org.springframework.stereotype.Service;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Service
 public class CrimeReportServiceImpl implements CrimeReportService {
@@ -17,20 +18,7 @@ public class CrimeReportServiceImpl implements CrimeReportService {
     }
 
     @Override
-    public CrimeReport addReport(CrimeReport report) {
-
-        if (report.getLatitude() < -90 || report.getLatitude() > 90) {
-            throw new IllegalArgumentException("latitude invalid");
-        }
-
-        if (report.getLongitude() < -180 || report.getLongitude() > 180) {
-            throw new IllegalArgumentException("longitude invalid");
-        }
-
-        if (report.getOccurredAt().isAfter(LocalDateTime.now())) {
-            throw new IllegalArgumentException("occurred time cannot be future");
-        }
-
+    public CrimeReport saveReport(CrimeReport report) {
         return reportRepository.save(report);
     }
 
